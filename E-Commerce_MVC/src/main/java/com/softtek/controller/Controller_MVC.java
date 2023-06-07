ccf51564e5f14fc92564ce3fc5f5722b1d7b6d12
Sagar1Hr <117274350+Sagar1Hr@users.noreply.github.com>
2023-06-07 14:47:41 +0530

    E-Commerce_MVC project Update
    Provided new Index page for the project using css and js

--- E-Commerce_MVC/src/main/java/com/softtek/controller/Controller_MVC.java
+++ E-Commerce_MVC/src/main/java/com/softtek/controller/Controller_MVC.java
@@ -7,6 +7,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
@@ -16,21 +17,30 @@ import com.softtek.entity.Product;
 @Controller
 public class Controller_MVC {
 
-	@RequestMapping("/")
-	public String showHome() {
-		return "home";
+	@GetMapping("/")
+	public String showHome(Map<String, Object> map) {
+		String videoPath1 = "/E-Commerce_MVC/src/main/webapp/WEB-INF/pages/videos/shopping.mp4";
+		map.put("videoPath1", videoPath1);
+		String videoPath2 = "/E-Commerce_MVC/src/main/webapp/WEB-INF/pages/videos/clothes.mp4";
+		map.put("videoPath2", videoPath2);
+		String videoPath3 = "/E-Commerce_MVC/src/main/webapp/WEB-INF/pages/videos/unboxing.mp4";
+		map.put("videoPath3", videoPath3);
+		String videoPath4 = "/E-Commerce_MVC/src/main/webapp/WEB-INF/pages/videos/happy.mp4";
+		map.put("videoPath4", videoPath4);
+		String videoPath5 = "/E-Commerce_MVC/src/main/webapp/WEB-INF/pages/videos/shopgo.mp4";
+		map.put("videoPath5", videoPath5);
+		return "index";
 	}
 
 	@GetMapping("/productList")
 	public String myPage(Map<String, Object> map) {
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<List<Product>> response = restTemplate.exchange("http://localhost:9905/api/product/get/productList",
-				HttpMethod.GET, null, new ParameterizedTypeReference<List<Product>>() {
+		ResponseEntity<List<Product>> response = restTemplate.exchange(
+				"http://localhost:9905/api/product/get/productList", HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<Product>>() {
 				});
 		List<Product> products = response.getBody();
 		products.stream().forEach(System.out::println);
-
-		map.put("proList", products);
 
 		return "product_list";
 	}
--- E-Commerce_MVC/src/main/webapp/WEB-INF/pages/home.jsp
+++
@@ -1,5 +0,0 @@
-<h1 style="color: red; text-align: center">Welcome to Home Page</h1>
-
-<h3 style="color: Green; text-align: center">Home Page</h3>
-<h3 style="text-align: center;"><a href="register">Register</a></h3>
-<h3 style="text-align: center;"><a href="productList">Product List</a></h3>
